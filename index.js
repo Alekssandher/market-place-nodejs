09ec08d1bbb98d836f8faedba799f4beea91d66c
Alekssandher <alekssandher1@hotmail.com>
2024-10-29 11:23:09 -0300

    Feat: database connection made

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const connectToDatabase = require('./src/database/database')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+connectToDatabase()
+
+app.get('/', (req, res) => {
+    res.send({message: 'rodando'})
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em https://localhost:${port}`)
+})
---
+++ src/database/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+function connectToDatabase() {
+    mongoose.connect('mongodb://localhost:27017', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    }).then(() => {
+        console.log("database connected")
+    }).catch((error) => {
+        return console.log(`something went wrong error: ${error}`)
+    });
+
+}
+
+module.exports = connectToDatabase

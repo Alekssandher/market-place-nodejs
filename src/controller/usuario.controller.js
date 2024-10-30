b2abfada2e8c316a7d62fa90c59e44c5fe2edf68
Alekssandher <alekssandher1@hotmail.com>
2024-10-30 10:44:13 -0300

    Feat: user controller and router updated

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,120 @@
+
+const findUserByIdController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const findAllUsersController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const createUserController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const updateUserController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const removeUserController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const addUserAddressController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const removeAddressController = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const addUserFavProduct = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+const removeUserFavProduct = async (req, res) => {
+    try {
+
+
+    }catch(error) {
+
+        console.log(`erro: ${error.message}`)
+        return res.status(500).send({message: 'internal error'})
+        
+    }
+}
+
+module.exports = {
+    findUserByIdController,
+    findAllUsersController,
+    createUserController,
+    updateUserController,
+    removeUserController,
+    addUserAddressController,
+    removeAddressController,
+    addUserFavProduct,
+    removeUserFavProduct
+}

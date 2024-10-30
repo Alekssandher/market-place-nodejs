aac725e41e0a3fb11d56839e7c7cf713ef629800
Alekssandher <alekssandher1@hotmail.com>
2024-10-30 08:36:53 -0300

    Feat: user router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectToDatabase = require('./src/database/database')
 
+const usuario = require('./src/router/usuario.router')
 const app = express()
 
 const port = 3000
@@ -8,6 +9,8 @@ const port = 3000
 app.use(express.json())
 
 connectToDatabase()
+
+app.use('/usuario', usuario)
 
 app.get('/', (req, res) => {
     res.send({message: 'rodando'})

6604acde398adbcc370c454bbe07e77999c3e956
Alekssandher <alekssandher1@hotmail.com>
2024-10-31 09:10:41 -0300

    Feat: controller implementations

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,9 +1,21 @@
+const userService = require('../service/usuario.service')
 
 const findUserByIdController = async (req, res) => {
     try {
+        const user = await userService.findUserByIdService(req.params.id)
 
+        if (!user) {
+            return res.status(404).send({message: "user not found, try again later"})
+        }
+
+        return res.status(200).send(user)
 
     }catch(error) {
+
+        if (error.kind == 'ObjectId') {
+            console.log(error.kind == 'ObjectId')
+            return res.status(400).send({message: 'ID incorreto'})
+        }
 
         console.log(`erro: ${error.message}`)
         return res.status(500).send({message: 'internal error'})
@@ -14,6 +26,7 @@ const findUserByIdController = async (req, res) => {
 const findAllUsersController = async (req, res) => {
     try {
 
+        return res.status(200).send(await userService.findAllUsersService())
 
     }catch(error) {
 
@@ -25,7 +38,12 @@ const findAllUsersController = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try {
+        const body = req.body
+        if (!body.nome) {
+            return res.status(400).send({message: 'campo nome está faltando'})
+        }
 
+        return res.status(201).send(await userService.createUserService(body))
 
     }catch(error) {
 
@@ -37,6 +55,7 @@ const createUserController = async (req, res) => {
 
 const updateUserController = async (req, res) => {
     try {
+        return res.status(202).send(await userService.updateUserService(req.params.id, req.body))
 
 
     }catch(error) {
@@ -49,7 +68,13 @@ const updateUserController = async (req, res) => {
 
 const removeUserController = async (req, res) => {
     try {
+        const deletedUser = await userService.removeUserService(req.params.id)
 
+        if (deletedUser.deletedCount > 0) {
+            return res.status(200).send({message: 'usuario removido com sucesso'})
+        } else {
+            return res.status(404).send({message: 'usuario não encontrado'})
+        }
 
     }catch(error) {
 

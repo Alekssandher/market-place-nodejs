aa016014daff6e94d8ffc94ef475cfcfb25d7f10
Alekssandher <alekssandher1@hotmail.com>
2024-12-20 14:43:40 -0300

    swagger

--- src/router/docs.router.js
+++ src/router/docs.router.js
@@ -3,7 +3,13 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('../../swagger.json');
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDoc));
+router.get("/api-docs", swaggerUi.setup(swaggerDoc, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL
+}));
 
 module.exports = router;
